7daab75e8ef5041b59737f79bd5d9a222bcec90b
Abdullah <abdullah@abdullah.today>
2019-10-31 19:36:50 +0500

    polybar with xresources and dependencies

--- bspwm/.config/bspwm/autostart.sh
+++ bspwm/.config/bspwm/autostart.sh
@@ -14,21 +14,21 @@ if [ -d /etc/X11/xinit/xinitrc.d ]; then
     unset f
 fi
 
+xrdb -load $HOME/.Xresources &
 $HOME/.config/polybar/launch.sh &
 xmodmap $HOME/.Xmodmap &
 compton -b --config $HOME/.config/compton/compton.conf &
 xsetroot -cursor_name left_ptr &
 #xcompmgr -c -f D 5 &
-xrdb -load $HOME/.Xresources &
 xset +fp $HOME/.local/share/fonts/ &
 xset fp rehash &
 urxvtd -q -o -f &
 unclutter -root -reset -idle 1 -noevents &
 xset dpms 180 &
-xss-lock -- slock &
+xss-lock -- $HOME/.local/bin/lock &
 dunst -c $HOME/.config/dunst/dunstrc &
 ~/.fehbg &
-xbacklight = 5 &
+xbacklight = 50 &
 pactl set-source-mute alsa_input.pci-0000_00_1b.0.analog-stereo true &
 #redshift -c ~/.config/redshift/redshift.conf &
 [ -z $TMUX ] && tmux new-session -s $USER -d 
