cfa866b1b3871cf94b20fd5b3b60ecaf812636f2
Abdullah <abdullah@abdullah.today>
2020-01-23 10:26:03 +0500

    adding mpv function, and vi-mode term option

--- zsh/.zsh/options.zsh
+++ zsh/.zsh/options.zsh
@@ -87,3 +87,25 @@ setopt complete_in_word
 
 # Glob complete
 setopt glob_complete
+
+#function zle-keymap-select {
+#  if [[ ${KEYMAP} == vicmd ]] ||
+#     [[ $1 = 'block' ]]; then
+#    echo -ne '\e[1 q'
+#  elif [[ ${KEYMAP} == main ]] ||
+#       [[ ${KEYMAP} == viins ]] ||
+#       [[ ${KEYMAP} = '' ]] ||
+#       [[ $1 = 'beam' ]]; then
+#    echo -ne '\e[3 q'
+#  fi
+#}
+#zle -N zle-keymap-select
+#
+#zle-line-init() {
+#    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+#    echo -ne "\e[3 q"
+#}
+#zle -N zle-line-init
+#
+#echo -ne '\e[3 q' # Use beam shape cursor on startup.
+#preexec() { echo -ne '\e[3 q' ;} #
