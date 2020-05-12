bdb6bd64f93f2f5b875b2d3e1323a602b3373e7d
Abdullah <abdullah@abdullah.today>
2020-05-12 23:43:59 +0500

    zsh prompt setup with powerlevel10k

--- zsh/.zsh/commands.zsh
+++ zsh/.zsh/commands.zsh
@@ -20,3 +20,13 @@ fi
 if [[ $commands[pacman] ]]; then
   alias pacman='sudo pacman'
 fi
+
+if [[ $commands[colorls] ]]; then
+  alias ls='colorls --almost-all --git-status --group-directories-first'
+else
+  alias ls='ls --color=auto'
+fi
+
+if [[ $commands[todo.sh] ]]; then
+  alias todo='todo.sh'
+fi
