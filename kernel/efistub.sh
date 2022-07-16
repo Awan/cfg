aed385c406154d1500af792ac510e6f5b17d9a77
Abdullah <abdullah@abdullah.today>
2022-07-16 21:01:35 +0500

    efistub script got changes.
    added old efistub script some changes like acpi and other stuff

--- kernel/efistub.sh
+++ kernel/efistub.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 # shellcheck disable=2034
 coded_by='
 
@@ -15,4 +15,4 @@ In the name of Allah, the most Gracious, the most Merciful.
 '
 
 
-efibootmgr --create --disk /dev/sda --part 1 --label "Abdullah's Encrypted ArchLinux" --loader /vmlinuz-linux --unicode 'cryptdevice=UUID=0914303a-e5ad-47f1-95ad-02fdbc469d25:cryptlvm root=UUID=2ac45312-1f1a-4af9-a744-00806a7cbc73 rw splash ipv6.disable=1 nmi_watchdog=0 consoleblank=60 acpi_backlight=thinkpad_acpi i915.i915_enable_fbc=1 i915.i915_enable_rc6=1 initrd=\intel-ucode.img initrd=\initramfs-linux.img resume=UUID=a65c01e6-084f-40d4-8304-cba022dff8ae' --verbose
+sudo efibootmgr --create --disk /dev/sda --part 1 --label "Abdullah's Encrypted ArchLinux" --loader /vmlinuz-linux --unicode 'cryptdevice=UUID=998b6b6f-c895-4e28-9c99-ec3e89a48ddc:cryptlvm root=UUID=87c40799-b141-48a5-a694-3384b0688f14 rw splash ipv6.disable=1 nmi_watchdog=0 consoleblank=60 acpi_backlight=thinkpad_acpi i915.i915_enable_fbc=1 i915.i915_enable_rc6=1 initrd=\intel-ucode.img initrd=\initramfs-linux.img resume=UUID=2bf653e3-e387-45b2-92ef-8740293859e2 psmouse-synaptics_intertouch=0 allow-discards' --verbose
