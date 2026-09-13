4760e6dc18d69b4ad42f769315cfd68d0df487fa
Muhammad Abdullah Khabir <abdullah@abdullah.support>
2026-09-13 16:46:36 +0500

    adding fonts updates\

--- kernel/efistub.sh
+++ kernel/efistub.sh
@@ -14,4 +14,4 @@ In the name of Allah, the most Gracious, the most Merciful.
 COMMENT
 
 
-sudo efibootmgr --create --disk /dev/sda --part 1 --label "Abdullah's Encrypted ArchLinux" --loader /vmlinuz-linux --unicode 'cryptdevice=UUID=998b6b6f-c895-4e28-9c99-ec3e89a48ddc:cryptlvm root=UUID=87c40799-b141-48a5-a694-3384b0688f14 rw splash ipv6.disable=1 nmi_watchdog=0 consoleblank=60 acpi_backlight=thinkpad_acpi i915.i915_enable_fbc=1 i915.i915_enable_rc6=1 initrd=\intel-ucode.img initrd=\initramfs-linux.img resume=UUID=2bf653e3-e387-45b2-92ef-8740293859e2 psmouse-synaptics_intertouch=0 allow-discards' --verbose
+sudo efibootmgr --create --disk /dev/sda --part 1 --label "Abdullah's Encrypted ArchLinux" --loader /vmlinuz-linux --unicode 'cryptdevice=UUID=998b6b6f-c895-4e28-9c99-ec3e89a48ddc:cryptlvm root=UUID=87c40799-b141-48a5-a694-3384b0688f14 rw splash ipv6.disable=1 nmi_watchdog=0 consoleblank=60 acpi_backlight=thinkpad_acpi initrd=\intel-ucode.img initrd=\initramfs-linux.img resume=UUID=2bf653e3-e387-45b2-92ef-8740293859e2 psmouse-synaptics_intertouch=0 allow-discards' --verbose
